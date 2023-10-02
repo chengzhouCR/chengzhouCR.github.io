61391a0cd12ed62b6ee9751fde85a09c6c1806f7
PolarBear-Zhou <2702902275@qq.com>
2023-10-02 22:31:04 +0800

    Site updated: 2023-10-02 22:31:00

---
+++ js/https.js
@@ -0,0 +1,8 @@
+var reg = /^(localhost)/;
+    if(window.location.hostname !== 'localhost' && !reg.test(window.location.hostname)) {
+      // 判断非本地server时 http强制转换成https
+	  var targetProtocol = "https:";
+      if (window.location.protocol != targetProtocol)
+      window.location.href = targetProtocol +
+      window.location.href.substring(window.location.protocol.length);
+    }
