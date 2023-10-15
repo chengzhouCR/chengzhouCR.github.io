54d9ce924eb5cafd5da17c5c7a2b9c70bdf64d56
PolarBear-Zhou <2702902275@qq.com>
2023-10-15 15:46:46 +0800

    Site updated: 2023-10-15 15:46:45

--- js/reload.js
+++
@@ -1,8 +0,0 @@
-$(document).pjax('a', '#pjax-container', {
-    fragment: '#pjax-container',
-    timeout: 8000,
-    success: function() {
-        //重新加载JS
-        $.getScript("cloudflare.js");
-    }
-});
